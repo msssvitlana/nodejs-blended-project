b249e01862386cebd8979b8d42f6521251953808
Svitlana <missis.chuvaeva@ukr.net>
2025-08-10 15:33:06 +0300

    task-4

--- src/routers/products.js
+++ src/routers/products.js
@@ -1,8 +1,12 @@
-import { Router } from "express";
-import { getAllProductsController } from "../controllers/products.js";
+import { Router } from 'express';
+import {
+  getAllProductsController,
+  getProductbyIdController,
+} from '../controllers/products.js';
 
 const productsRouter = Router();
 
 productsRouter.get('/products', getAllProductsController);
 
+productsRouter.get('/products/:productId', getProductbyIdController);
 export default productsRouter;
--- src/services/products.js
+++ src/services/products.js
@@ -1,7 +1,13 @@
-import { ProductModel } from "../db/models/product.js"
+import { ProductModel } from '../db/models/product.js';
 
 export const getAllProducts = async () => {
-    const products = await ProductModel.find();
+  const products = await ProductModel.find();
 
-    return products;
-}
+  return products;
+};
+
+export const getProductbyId = async (productId) => {
+  const product = await ProductModel.findById(productId);
+
+  return product;
+};
